5546e8b817e028446de0353689a1e0164d5736a9
cantux <can.tuksavul@gmail.com>
2019-05-16 10:21:12 +0000

    scrap command updated for ubuntu ec2

--- scrap_now.sh
+++ scrap_now.sh
@@ -1,9 +1,13 @@
-#/bin/bash
+#/bin/sh
 
-echo "before calling source: $PATH"
-source /home/ubuntu/miniconda2/bin/activate py_scrap
-echo "after calling source: $PATH"
-python main.py
-mv data* /var/www/html/.
-source deactivate
-
+#source ~/miniconda2/etc/profile.d/conda.sh
+#conda init zsh
+#source /home/ubuntu/miniconda2/bin/activate /home/ubuntu/miniconda2/envs/py_scrap
+#conda activate /home/ubuntu/miniconda2/envs/py_scrap
+/home/ubuntu/miniconda2/condabin/conda run -p /home/ubuntu/miniconda2/envs/py_scrap python /home/ubuntu/carr_parr/main.py 
+#conda run -n py_scrap python /home/ubuntu/carr_parr/main.py 
+#python /home/ubuntu/carr_parr/main.py 
+#/home/ubuntu/miniconda2/bin/python /home/ubuntu/carr_parr/main.py
+mv /home/ubuntu/data* /var/www/html/.
+#source /home/ubuntu/miniconda2/bin/deactivate
+#conda deactivate
