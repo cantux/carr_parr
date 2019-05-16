6edd51f64356465d3f87e170b765174132d37c6e
cantux <can.tuksavul@gmail.com>
2019-05-16 00:44:00 -0700

    updated for new site wwith generator

--- main.py
+++ main.py
@@ -4,17 +4,46 @@ from bs4 import BeautifulSoup
 import csv
 
 import re
+import datetime
 
 class Ser():
     pass
 
-def get_item_count(cat_soup):
-    pagination_bar = cat_soup.find('div', attrs={'class': 'pagination-bar'})
-    raw_item_count = pagination_bar.find('div', attrs={'class': "total-pr-col"})
-    item_count = re.search(r"\d+", raw_item_count.text).group(0)
-    return int(item_count)
+# following is depracated with new site design.
+# item_count = get_item_count(cat_soup)
+# page_count = int(item_count / 24) + 2
+# def get_item_count(cat_soup):
+#     pagination_bar = cat_soup.find('div', attrs={'class': 'pagination-bar'})
+#     raw_item_count = pagination_bar.find('div', attrs={'class': "total-pr-col"})
+#     item_count = re.search(r"\d+", raw_item_count.text).group(0)
+#     return int(item_count)
 
-def append_items(products, cat_soup, cat_type, cat_link):
+class ProductRetriever_gen():
+    def __init__(self, cat_link):
+        self.cat_link = cat_link
+        self.num = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        response = requests.get(self.cat_link + '?page={}'.format(self.num))
+        cat_page = response.text
+        cat_soup = BeautifulSoup(cat_page, 'html.parser')
+ 
+        if not check_no_product_found(cat_soup): 
+            self.num += 1
+            return (cat_soup, self.cat_link)
+        else:
+            raise StopIteration()
+
+def check_no_product_found(soup):
+    return soup.find('div', attrs={'id': 'noProductFound'}) is not None
+
+def append_items(products, cat_soup, cat_link):
     product_list = cat_soup.find('ul', attrs={'class': 'product-listing product-grid container-fluid add_to_cart'})
     for product in product_list.find_all('li'):
         s = Ser()
@@ -25,34 +54,21 @@ def append_items(products, cat_soup, cat_type, cat_link):
         s.product_main_category = product.find('input', attrs={"id": "productMainCategoryPost"})['value']
         s.product_link = product.find('a')['href']
         s_dict = s.__dict__
-        s_dict.update({"category": cat_type, "category_link": cat_link})
-        # print("items:" + str(s_dict))
+        s_dict.update({"category_link": cat_link})
+        print("items:" + str(s_dict))
         products.append(s_dict)
 
 def parse(category_dict):
     for (cat_type, cat_link) in category_dict.items():
-        response = requests.get(cat_link)
-        cat_page = response.text
-        # cat_page = urllib.request.urlretrieve(cat_link)
-
-        cat_soup = BeautifulSoup(cat_page, 'html.parser')
 
         products = list()
-        # get first page
-        append_items(products, cat_soup, cat_type, cat_link)
 
-        item_count = get_item_count(cat_soup)
-        page_count = int(item_count / 24) + 2
-        print("page count: " + str(page_count))
-        for page_number in range(2, page_count):
-            print("current: " + str(page_number))
-            response = requests.get(cat_link + '?page={}'.format(page_number))
-            cat_page = response.text
-            cat_soup = BeautifulSoup(cat_page, 'html.parser')
-            append_items(products, cat_soup, cat_type, cat_link)
+        for (cat_soup, cat_link) in ProductRetriever_gen(cat_link):
+           print("current: " + str(cat_link))
+           append_items(products, cat_soup, cat_link)
 
         # open a csv file with append, so old data will not be erased
-        with open('index.csv', 'a') as csv_file:
+        with open('data_{0}_{1}.csv'.format(cat_type, datetime.datetime.now()), 'a') as csv_file:
             writer = csv.writer(csv_file)
             for p in products:
                 writer.writerow(p.values())
@@ -68,3 +84,7 @@ if __name__ == "__main__":
     }
 
     parse(category_dict)
+
+#     for (i, x) in enumerate(ProductRetriever_gen("https://www.carrefoursa.com/tr/meyve-sebze/c/1014")):
+#         print(i)
+
